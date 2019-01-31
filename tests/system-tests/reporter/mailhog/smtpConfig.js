469ca45900060fd5ca05f8b64e3e1037eac4baea
lipetzniv <niv.lipetz@zooz.com>
2019-01-31 12:07:42 +0200

    test(reporter): add system tests

---
+++ tests/system-tests/reporter/mailhog/smtpConfig.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var config = {
+    host: process.env.MAILHOG_HOST,
+    port: process.env.MAILHOG_PORT
+};
+
+module.exports = config;
---
+++ tests/system-tests/reporter/requestCreator.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const request = require('supertest');
+
+const URL = process.env.URL || 'http://localhost:8080';
+const HEADERS = {'x-zooz-request-id': 'mickey', 'Content-Type': 'application/json'};
+
+module.exports = {
+    createReport,
+    postStats,
+    getReport,
+    getReports,
+    getLastReports,
+    getHTMLReport
+};
+
+function createReport(testId, body) {
+    return request(URL).post(`/v1/tests/${testId}/reports`)
+        .send(body)
+        .set(HEADERS)
+        .expect(function(res){
+            return res;
+        });
+}
+
+function postStats(testId, reportId, body) {
+    return request(URL).post(`/v1/tests/${testId}/reports/${reportId}/stats`)
+        .send(body)
+        .set(HEADERS)
+        .expect(function(res){
+            return res;
+        });
+}
+
+function getReport(testId, reportId) {
+    return request(URL).get(`/v1/tests/${testId}/reports/${reportId}`)
+        .set(HEADERS)
+        .expect(function(res){
+            return res;
+        });
+}
+
+function getReports(testId) {
+    return request(URL).get(`/v1/tests/${testId}/reports`)
+        .set(HEADERS)
+        .expect(function(res){
+            return res;
+        });
+}
+
+function getLastReports(limit) {
+    return request(URL).get(`/v1/tests/last_reports?limit=${limit}`)
+        .set(HEADERS)
+        .expect(function(res){
+            return res;
+        });
+}
+
+function getHTMLReport(testId, reportId) {
+    return request(URL).get(`/v1/tests/${testId}/reports/${reportId}/html`)
+        .set(HEADERS)
+        .expect(function(res){
+            return res;
+        });
+}
