deb4451e78056f17cbb75a113ea5e1670476fd0c
NivLipetz <31369413+NivLipetz@users.noreply.github.com>
2019-02-11 16:13:55 +0200

    Release (#39)
    * chore: fix release stage
    
    * chore: fix circleci workflow
    
    * refactor(pipeline): rename hold stage
    
    * chore(pipeline): add ssh key for github access
    
    * chore(pipeline): release only on master
    
    * refactor(pipeline): change tag message

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -e
 
-if [ $CIRCLE_BRANCH != "master" ] ; then
-    exit 0
-fi
-
 TAG=v$(node -p "require('./package.json').version")
 
 echo "Releasing tag: $TAG"
 
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
+git config --global user.email $GIT_EMAIL
+git config --global user.name $GIT_USER
 
 CONTAINER_IMAGE=zooz/predator:latest
 docker pull $CONTAINER_IMAGE
@@ -17,7 +15,7 @@ docker tag $CONTAINER_IMAGE $TAGGED_DOCKER_IMAGE
 docker push $TAGGED_DOCKER_IMAGE
 
 # Release the project tag
-git tag -a $TAG -m "New predator version="$TAG""
+git tag -a $TAG -m "New release: "$TAG""
 
 # Push the commit tag to gitlab
 git push --follow-tags origin master
