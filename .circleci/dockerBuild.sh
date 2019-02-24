11c59ba1f53e6e4c3c854db917f1b83d18dba11c
NivLipetz <31369413+NivLipetz@users.noreply.github.com>
2019-02-24 15:08:25 +0200

    chore(pipeline): push development images into zooz/predator-builds repo (#68)

--- .circleci/dockerBuild.sh
+++ .circleci/dockerBuild.sh
@@ -1,16 +1,17 @@
 #!/bin/sh -e
 
 if [ "$CIRCLE_BRANCH" != "master" ] ; then
-    export TAG=branches-$CIRCLE_BRANCH
+    export TAG=predator-$CIRCLE_BRANCH
+    export IMAGE=zooz/predator-builds:$TAG
 else
     export TAG=latest
-
+    export IMAGE=zooz/predator:$TAG
 fi
 
-echo "Building Docker image for tag: $TAG on branch: $CIRCLE_BRANCH"
+echo "Building Docker image $IMAGE on branch: $CIRCLE_BRANCH"
 
-docker build -t zooz/predator:$TAG .
+docker build -t $IMAGE .
 
 echo $DOCKERHUB_PASSWORD | docker login -u $DOCKERHUB_USERNAME --password-stdin
 
-docker push zooz/predator:$TAG
+docker push $IMAGE
