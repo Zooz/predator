5ada800917d3814578c44b3cc1b34c28b8aed6fa
manorlh <44364426+manorlh@users.noreply.github.com>
2019-02-25 11:36:53 +0200

    refactor(rename env): rename env (#72)

--- ui/src/App/common/env.js
+++ ui/src/App/common/env.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-  PERFORMANCE_FRAMEWORK_BUCKET_PATH: (process.env.NODE_ENV === 'production') ? 'predator' : '',
-  PERFORMANCE_FRAMEWORK_API_URL: process.env.PERFORMANCE_FRAMEWORK_API_URL || 'http://predator.performance-framework.dcos-internal.qa-fra-apps.zooz.co/v1',
+  PREDATOR_BUCKET_PATH: (process.env.NODE_ENV === 'production') ? 'predator' : '',
+  PREDATOR_URL: process.env.PREDATOR_URL || 'http://predator.performance-framework.dcos-internal.qa-fra-apps.zooz.co/v1',
 };
