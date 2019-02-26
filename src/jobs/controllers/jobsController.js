87423847db742334e6125ef91e25375063963bc1
or.frenkel <or@zooz.com>
2019-02-26 17:06:55 +0200

    refactor(jobs): change using of handle error

--- src/jobs/controllers/jobsController.js
+++ src/jobs/controllers/jobsController.js
@@ -1,77 +1,64 @@
 'use strict';
 
-let logger = require('../../common/logger');
 let jobManager = require('../models/jobManager');
 
-module.exports.createJob = function(req, res) {
+module.exports.createJob = function(req, res, next) {
     return jobManager.createJob(req.body)
         .then(function(result){
             return res.status(201).json(result);
         })
         .catch(function(err){
-            return handleError(err, res);
+            return next(err, res);
         });
 };
 
-module.exports.getJobs = function(req, res) {
+module.exports.getJobs = function(req, res, next) {
     let shouldGetAllJobs = (req.query && (req.query.one_time === true || req.query.one_time === 'true'));
     return jobManager.getJobs(shouldGetAllJobs)
         .then(function(result) {
             return res.status(200).json(result);
         })
         .catch(function(err){
-            return handleError(err, res);
+            return next(err);
         });
 };
 
-module.exports.getJob = function(req, res) {
+module.exports.getJob = function(req, res, next) {
     return jobManager.getJob(req.params.job_id)
         .then(function(result) {
             return res.status(200).json(result);
-        })
-        .catch(function(err){
-            return handleError(err, res);
-        });
-};
-
-module.exports.updateJob = function(req, res) {
-    return jobManager.updateJob(req.params.job_id, req.body)
-        .then(function(result) {
-            return res.status(200).json(result);
-        })
-        .catch(function(err){
-            return handleError(err, res);
-        });
-};
-
-module.exports.deleteJob = function(req, res) {
-    return jobManager.deleteJob(req.params.job_id)
-        .then(function(){
-            return res.status(204).json();
-        })
-        .catch(function(err){
-            return handleError(err, res);
-        });
-};
-
-module.exports.stopRun = function (req, res, next) {
-    return jobManager.stopRun(req.params.job_id, req.params.run_id)
-        .then(function(){
-            return res.status(204).json();
         })
         .catch(function(err){
             return next(err);
         });
 };
 
-function handleError(err, res) {
-    if (err.statusCode) {
-        return res.status(err.statusCode).json({ message: err.message });
-    }
+module.exports.updateJob = function (req, res, next) {
+    return jobManager.updateJob(req.params.job_id, req.body)
+        .then(function (result) {
+            return res.status(200).json(result);
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+};
 
-    if (err.message === 'Error' || err.message === '' || err.message === undefined) {
-        err.message = 'Internal error occurred';
-    }
-    logger.error(err.message);
-    return res.status(500).json({ message: err.message });
-}
+module.exports.deleteJob = function (req, res, next) {
+    return jobManager.deleteJob(req.params.job_id)
+        .then(function () {
+            return res.status(204).json();
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+};
+
+module.exports.stopRun = function (req, res, next) {
+    return jobManager.stopRun(req.params.job_id, req.params.run_id)
+        .then(function () {
+            return res.status(204).json();
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+};
