7f8e1eee57fa24f0224aca1635bc9634fc64b702
or.frenkel <or@zooz.com>
2019-03-05 17:56:49 +0200

    test(config): add smtp config data to tests config file

--- tests/configurations/commonConfiguration.sh
+++ tests/configurations/commonConfiguration.sh
@@ -8,4 +8,9 @@ export MY_ADDRESS=$URL
 export MAILHOG_HOST=$RUNNER_IP
 export MAILHOG_PORT=8025
 
+export SMTP_HOST=$RUNNER_IP
+export SMTP_PORT=1025
+export SMTP_PASSWORD=password
+export SMTP_USERNAME=username
+
 export INTERNAL_ADDRESS='http://localhost:80'
