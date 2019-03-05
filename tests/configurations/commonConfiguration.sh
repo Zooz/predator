a52e0466dc9f5a7a977abec387fa03de7688ecdc
or.frenkel <or@zooz.com>
2019-03-05 17:40:08 +0200

    test(config): fix tests config files

--- tests/configurations/commonConfiguration.sh
+++ tests/configurations/commonConfiguration.sh
@@ -4,14 +4,10 @@ export RUNNER_IP=127.0.0.1
 export URL=$RUNNER_IP:80
 
 export MY_ADDRESS=$URL
-export GRAFANA_URL=https://grafana.zooz.com/predator
 
 export MAILHOG_HOST=$RUNNER_IP
 export MAILHOG_PORT=8025
 
-export SMTP_HOST=$RUNNER_IP
-export SMTP_PORT=1025
-export SMTP_PASSWORD=password
-export SMTP_USERNAME=username
+
 export DOCKER_NAME='zooz/predator-runner:latest'
-export INTERNAL_ADDRESS='http://localhost:80'
+export INTERNAL_ADDRESS='http://localhost:80'k
