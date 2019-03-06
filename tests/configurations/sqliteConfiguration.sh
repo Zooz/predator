bdb5ce0b69f2887cc8d38c705a5712bd3bf3689d
or.frenkel <or@zooz.com>
2019-03-06 11:01:30 +0200

    test(config): change db name for sqlite

--- tests/configurations/sqliteConfiguration.sh
+++ tests/configurations/sqliteConfiguration.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
 export DATABASE_TYPE=SQLITE
-export DATABASE_NAME=predator_sqlite_$( date +%T )
+export DATABASE_NAME=predator
 export DATABASE_ADDRESS=localhost
 export DATABASE_USERNAME=root
 export DATABASE_PASSWORD=password
-export SQLITE_STORAGE=./data.sqlite
+export SQLITE_STORAGE=predator_sqlite_$( date +%T )
