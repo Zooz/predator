7cd8c5fd68e47c3cc1920285aaf926109b8ca96d
or.frenkel <or@zooz.com>
2019-03-10 19:19:42 +0200

    fix(config): add swagger validator

--- src/configManager/routes/configRoute.js
+++ src/configManager/routes/configRoute.js
@@ -6,6 +6,6 @@ const config = require('../controllers/configController');
 const router = express.Router();
 
 router.put('/', swaggerValidator.validate, config.updateConfig);
-router.delete('/:key', config.deleteConfig);
+router.delete('/:key', swaggerValidator.validate, config.deleteConfig);
 router.get('/', config.getConfig);
 module.exports = router;
