8c8b5e426df67749f034dffba57067bc0ddd0696
or.frenkel <or@zooz.com>
2019-03-11 17:47:11 +0200

    test(config): change config to use env variables in tests

--- src/jobs/models/metronome/jobTemplate.js
+++ src/jobs/models/metronome/jobTemplate.js
@@ -1,10 +1,10 @@
-module.exports.createJobRequest = async (jobName, runId, parallelism, environmentVariables, dockerImage, configData) => {
+module.exports.createJobRequest = (jobName, runId, parallelism, environmentVariables, dockerImage, configData) => {
     return {
         id: jobName,
         description: 'Runs a performance test',
         run: {
-            cpus: await configData['runner_cpu'] || 1,
-            mem: await configData['runner_memory'] || 2048,
+            cpus: configData['runner_cpu'] || 1,
+            mem: configData['runner_memory'] || 2048,
             disk: 0,
             maxLaunchDelay: 30,
             docker: {
