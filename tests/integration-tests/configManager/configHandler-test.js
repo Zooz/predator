84c912d49b9f86472509386bec5ceeef9cc6e539
or.frenkel <or@zooz.com>
2019-03-18 18:04:02 +0200

    test(config): fix integration tests

--- tests/integration-tests/configManager/configHandler-test.js
+++ tests/integration-tests/configManager/configHandler-test.js
@@ -25,6 +25,7 @@ const updateBodyWithTypes = {
         buckets_sizes: 'string_value'
     },
     smtp_server: {
+        from: 'test@mail.com',
         host: 'string_value',
         port: 2,
         username: 'string_value',
@@ -56,6 +57,7 @@ const requestBody =
             buckets_sizes: 'string_value_buckets_sizes'
         },
         smtp_server: {
+            from:'test@mail.com',
             host: 'string_value_smtp_server',
             port: 2,
             username: 'string_value_username',
