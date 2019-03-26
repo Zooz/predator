ec605fd8a42e9163a6052c19893db65d278e9a7c
Manor <manor.lahagani@zooz.com>
2019-03-26 22:54:38 +0200

    feat(new ui): new ui
    new ui

--- ui/src/features/PerformanceUI/mainMenu.js
+++ ui/src/features/PerformanceUI/mainMenu.js
@@ -1,38 +1,37 @@
+import {faVial,faBriefcase,faFlagCheckered,faFileAlt, faCloudDownloadAlt, faStopCircle, faTrashAlt, faPen} from '@fortawesome/free-solid-svg-icons'
+
 module.exports = [
   {
     key: 0,
     primaryText: 'Tests',
-    nestedItems: [
-      {
-        key: 0,
-        primaryText: 'View tests',
-        navigateTo: 'tests'
-      }]
+    navigateTo: 'tests',
+    icon:faVial
   },
   {
     key: 1,
-    primaryText: 'Jobs',
-    nestedItems: [
-      {
-        key: 0,
-        primaryText: 'View Jobs',
-        navigateTo: 'jobs'
-      }]
+    primaryText: 'Scheduled Jobs',
+    navigateTo: 'jobs',
+    icon: faBriefcase
   },
   {
     key: 2,
-    primaryText: 'Reports',
-    nestedItems: [
-      {
-        key: 0,
-        primaryText: 'Last Reports',
-        navigateTo: 'last_reports'
-      }]
+    primaryText: 'Last Reports',
+    navigateTo: 'last_reports',
+    icon:faFlagCheckered
   },
   {
     key: 3,
     primaryText: 'Docs',
+    icon:faFileAlt,
     nestedItems: [
-      ]
+      {
+        key: 0,
+        primaryText: 'Documentation',
+        linkUrl: 'https://zooz.github.io/predator/index'
+      }, {
+        key: 1,
+        primaryText: 'API Reference',
+        linkUrl: 'https://zooz.github.io/predator/#indexapiref.html'
+      }]
   }
 ];
