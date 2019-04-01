dbcf518436631c3592f914623ff76435d531c10b
Eli Nudler <enudler@gmail.com>
2019-04-01 15:09:36 -0500

    refactor(dbconfig): DATABASE_NAME default is now 'predator'

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,7 +1,7 @@
 let cassandra = require('cassandra-driver');
 const config = {
     type: (process.env.DATABASE_TYPE || 'SQLITE').toUpperCase(),
-    name: process.env.DATABASE_NAME,
+    name: process.env.DATABASE_NAME || 'predator',
     address: process.env.DATABASE_ADDRESS,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
