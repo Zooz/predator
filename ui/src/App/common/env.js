0cd93a774c24eafaa4773ec9af1dbb052fe9500f
Manor <manor.lahagani@zooz.com>
2019-04-05 21:58:40 +0300

    fix

--- ui/src/App/common/env.js
+++ ui/src/App/common/env.js
@@ -1,25 +1,4 @@
 module.exports = {
-    PREDATOR_URL: generatePredatorUrl(),
-    BUCKET_PATH: generateBucketPath()
+    PREDATOR_URL: process.env.PREDATOR_URL,
+    BUCKET_PATH: 'predator/'
 };
-
-function generatePredatorUrl() {
-    if (process.env.PREDATOR_URL){
-        return process.env.PREDATOR_URL;
-    }
-    if (process.env.BUCKET_PATH) {
-        return `${process.env.BUCKET_PATH}/v1`;
-    }
-    return '/v1';
-}
-
-function generateBucketPath() {
-    let path = '/ui/';
-    if (process.env.NODE_ENV !== 'production'){
-        path = '';
-    }
-    if (process.env.BUCKET_PATH) {
-        path = `${process.env.BUCKET_PATH}/ui/`;
-    }
-    return path;
-}
