351a39cc2bdbf16c334229d07cf8c338b569a70a
OrFrenkelZooz <48205818+OrFrenkelZooz@users.noreply.github.com>
2019-05-20 13:20:37 +0300

    test(processor): fix tests table schema (#140)
    fix(db): fix tests table schema

---
+++ src/database/sequlize-handler/migrations/01_tests_file_id.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+module.exports.up = async (query, DataTypes) => {
+    let testsTable = await query.describeTable('tests');
+
+    if (!testsTable.file_id) {
+        await query.addColumn(
+            'tests', 'file_id',
+            Sequelize.DataTypes.UUID);
+    }
+};
+
+module.exports.down = async (query, DataTypes) => {
+    await query.removeColumn('tests', 'file_id');
+};
