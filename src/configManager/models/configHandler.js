2b4d87f8ab4edf04e61a9640ae5d379f1d6c9c6a
or.frenkel <or@zooz.com>
2019-06-27 11:31:18 +0300

    test(processor): fix config value parse

--- src/configManager/models/configHandler.js
+++ src/configManager/models/configHandler.js
@@ -1,10 +1,13 @@
 const dbConnector = require('./database/databaseConnector');
 const configDataMap = require('../helpers/configDataMap');
 const configTemplate = require('../../common/consts').CONFIG;
+const convertData = require('../../helpers/convertData');
 
 module.exports.getConfigValue = async (configPath) => {
     const dbConfigValue = await dbConnector.getConfigValue(configPath);
-    return dbConfigValue || configDataMap.getConstDefaultValue(configPath);
+    const type = configDataMap.getConstType(configPath);
+    const value = dbConfigValue || configDataMap.getConstDefaultValue(configPath);
+    return convertData.convertByType(value, type);
 };
 
 module.exports.getConfig = async () => {
@@ -24,9 +27,10 @@ module.exports.deleteConfig = async (key) => {
 
 function createConfigObject(configAsObject) {
     let config = {};
-    Object.values(configTemplate).forEach(configTemplateKeys => {
-        const value = configAsObject[configTemplateKeys] !== (undefined) ? configAsObject[configTemplateKeys] : configDataMap.getConstDefaultValue(configTemplateKeys);
-        config[configTemplateKeys] = value;
+    Object.values(configTemplate).forEach(configTemplateKey => {
+        const value = configAsObject[configTemplateKey] !== (undefined) ? configAsObject[configTemplateKey] : configDataMap.getConstDefaultValue(configTemplateKey);
+        const type = configDataMap.getConstType(configTemplateKey);
+        config[configTemplateKey] = convertData.convertByType(value, type);
     });
     return config;
 }
