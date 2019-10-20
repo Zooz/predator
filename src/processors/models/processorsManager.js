6d925df67b19e847022b232cd45352316efec2d1
Niv Lipetz <niv.lipetz@gmail.com>
2019-10-20 21:22:25 +0300

    WIP: feat(custom-js): implement POST /processors (#213)
    * docs(image): fix outdated image in docs (#214)
    
    * chore(ci/cd): remove circleci docker_layer_caching (#218)
    
    * feat(api): add api spec for processors resource (#200)
    
    * feat(api): add api spec for processors resource
    
    * feat(api): add api spec for processors resource
    
    closes #185
    
    fix #185
    
    * fix(typo): typo date_time to date-time
    
    * fix(typo): typo date_time to date-time
    
    * chore(openapi3): code review comments addressed
    
    * docs(api): add download endpoint for processor files
    
    * chore(dependencies): update cassandra-driver to v4.1.0 (#210)
    
    * chore: fix rebase to master
    
    * feat(custom-js): implement POST /processors
    
    * test(processors): add tests for POST /processors (#217)
    
    * test(processors): add tests for POST /processors
    
    * style(processors): fix processors cassandra connector name
    
    * feat(processors): add js validation on POST /processors
    
    * fix(processors): add updated_at/created_at to processors resource
    
    * test(processors): add validateJavascriptContent unit-tests
    
    * test(create-test): fix processor file download error message in test

---
+++ src/processors/models/processorsManager.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const uuid = require('uuid');
+
+const logger = require('../../common/logger'),
+    databaseConnector = require('./database/databaseConnector'),
+    common = require('../../common/consts.js'),
+    fileManager = require('../../tests/models/fileManager.js');
+
+module.exports.createProcessor = async function (processor) {
+    let processorId = uuid.v4();
+    try {
+        if (processor.type === common.PROCESSOR_TYPE_FILE_DOWNLOAD) {
+            processor.javascript = await fileManager.downloadFile(processor.file_url);
+        }
+        fileManager.validateJavascriptContent(processor.javascript);
+        await databaseConnector.insertProcessor(processorId, processor);
+        logger.info('Processor saved successfully to database');
+        return processor;
+    } catch (error) {
+        logger.error(error, 'Error occurred trying to create new processor');
+        return Promise.reject(error);
+    }
+};
