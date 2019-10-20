72cce5d26a15dfb7f3973158fbdc980ea55d3b1a
Niv Lipetz <niv.lipetz@gmail.com>
2019-10-20 21:22:25 +0300

    WIP: feat(custom-js): implement POST /processors (#213)
    * docs(image): fix outdated image in docs (#214)
    
    * chore(ci/cd): remove circleci docker_layer_caching (#218)
    
    * feat(api): add api spec for processors resource (#200)
    
    * feat(api): add api spec for processors resource
    
    * feat(api): add api spec for processors resource
    
    closes #185
    
    fix #185
    
    * fix(typo): typo date_time to date-time
    
    * fix(typo): typo date_time to date-time
    
    * chore(openapi3): code review comments addressed
    
    * docs(api): add download endpoint for processor files
    
    * chore(dependencies): update cassandra-driver to v4.1.0 (#210)
    
    * chore: fix rebase to master
    
    * feat(custom-js): implement POST /processors
    
    * test(processors): add tests for POST /processors (#217)
    
    * test(processors): add tests for POST /processors
    
    * style(processors): fix processors cassandra connector name
    
    * feat(processors): add js validation on POST /processors
    
    * fix(processors): add updated_at/created_at to processors resource
    
    * test(processors): add validateJavascriptContent unit-tests
    
    * test(create-test): fix processor file download error message in test

---
+++ src/processors/models/database/sequelize/sequelizeConnector.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+let client;
+
+module.exports = {
+    init,
+    insertProcessor
+};
+
+async function init(sequelizeClient) {
+    client = sequelizeClient;
+    await initSchemas();
+}
+
+async function insertProcessor(processorId, processorInfo) {
+    const processor = client.model('processor');
+    let params = {
+        processor_id: processorId,
+        name: processorInfo.name,
+        description: processorInfo.description,
+        type: processorInfo.type,
+        file_url: processorInfo.file_url,
+        javascript: processorInfo.javascript,
+        created_at: Date.now(),
+        updated_at: Date.now()
+    };
+    return processor.create(params);
+}
+
+async function initSchemas() {
+    const processorsFiles = client.define('processor', {
+        processor_id: {
+            type: Sequelize.DataTypes.UUID,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.DataTypes.TEXT('medium')
+        },
+        description: {
+            type: Sequelize.DataTypes.TEXT('long')
+        },
+        type: {
+            type: Sequelize.DataTypes.TEXT('medium')
+        },
+        file_url: {
+            type: Sequelize.DataTypes.TEXT('long')
+        },
+        javascript: {
+            type: Sequelize.DataTypes.TEXT('long')
+        },
+        created_at: {
+            type: Sequelize.DataTypes.DATE
+        },
+        updated_at: {
+            type: Sequelize.DataTypes.DATE
+        }
+    });
+    await processorsFiles.sync();
+}
