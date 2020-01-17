76ade8d9b625807ea78986848816b9e336a89c03
Eli Nudler <enudler@gmail.com>
2020-01-17 11:28:56 +0200

    fix(processors): update processor ui sentence

--- ui/src/features/get-processors.js
+++ ui/src/features/get-processors.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Snackbar from 'material-ui/Snackbar';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import * as Selectors from './redux/selectors/processorsSelector';
 import style from './style.scss';
 import * as Actions from './redux/action';
@@ -8,10 +8,10 @@ import Loader from './components/Loader';
 import DeleteDialog from './components/DeleteDialog';
 import Page from '../components/Page';
 import ProcessorForm from './components/ProcessorForm';
-import {ReactTableComponent} from "../components/ReactTable";
-import {getColumns} from "./configurationColumn";
+import { ReactTableComponent } from '../components/ReactTable';
+import { getColumns } from './configurationColumn';
 import Button from '../components/Button';
-import _ from "lodash";
+import _ from 'lodash';
 
 /*
 *  TODO and error handling for delete processor
@@ -20,7 +20,7 @@ import _ from "lodash";
 const noDataMsg = 'There is no data to display.';
 const errorMsgGetProcessors = 'Error occurred while trying to get all processors.';
 const columnsNames = ['processor_name', 'description', 'updated_at', 'processor_edit', 'delete'];
-const DESCRIPTION = 'Processors are the way you can...';
+const DESCRIPTION = 'Processors inject custom JavaScript into test flows, allowing the flow to be as flexible as possible.';
 
 class getProcessors extends React.Component {
     constructor(props) {
@@ -34,43 +34,41 @@ class getProcessors extends React.Component {
             processorForEdit: null,
             sortedProcessors: [],
             sortHeader: ''
-        }
+        };
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.processorsList !== this.props.processorsList) {
-            this.setState({sortedProcessors: [...this.props.processorsList], sortHeader: 'updated_at-'}, () => {
+            this.setState({ sortedProcessors: [...this.props.processorsList], sortHeader: 'updated_at-' }, () => {
                 this.onSort('updated_at');
-            })
+            });
         }
     }
-
 
     onDelete = (data) => {
         this.setState({
             deleteDialog: true,
             processorToDelete: data
-        })
+        });
     }
     onSearch = (value) => {
         if (!value) {
-            this.setState({sortedProcessors: [...this.props.processorsList]})
+            this.setState({ sortedProcessors: [...this.props.processorsList] });
         }
         const newSorted = _.filter(this.props.processorsList, (processor) => {
             return (
                 _.includes(String(processor.name).toLowerCase(), value.toLowerCase()) ||
                 _.includes(String(processor.description).toLowerCase(), value.toLowerCase())
-            )
-
+            );
         });
-        this.setState({sortedProcessors: newSorted})
+        this.setState({ sortedProcessors: newSorted });
     };
 
     onSort = (field) => {
-        const {sortHeader} = this.state;
+        const { sortHeader } = this.state;
         let isAsc = false;
         if (sortHeader.includes(field)) {
-            isAsc = !sortHeader.includes('+')
+            isAsc = !sortHeader.includes('+');
         } else {
             isAsc = true;
         }
@@ -80,9 +78,8 @@ class getProcessors extends React.Component {
         } else {
             sortedReport = _.chain(this.state.sortedProcessors).sortBy(field).value();
         }
-        this.setState({sortedProcessors: sortedReport, sortHeader: `${field}${isAsc ? '+' : '-'}`})
+        this.setState({ sortedProcessors: sortedReport, sortHeader: `${field}${isAsc ? '+' : '-'}` });
     };
-
 
     submitDelete = () => {
         this.props.deleteProcessor(this.state.processorToDelete.id);
@@ -97,9 +94,8 @@ class getProcessors extends React.Component {
         });
     };
 
-
     onEdit = (data) => {
-        this.setState({createProcessor: true, processorForEdit: data});
+        this.setState({ createProcessor: true, processorForEdit: data });
     };
 
     closeCreateTest = () => {
@@ -109,7 +105,6 @@ class getProcessors extends React.Component {
         });
     };
 
-
     handleSnackbarClose = () => {
         this.props.setDeleteProcessorSuccess(false);
         this.setState({
@@ -118,8 +113,8 @@ class getProcessors extends React.Component {
     };
 
     loader() {
-        return this.props.processorLoading && this.props.processorsList && this.props.processorsList.length === 0 ?
-            <Loader/> : noDataMsg
+        return this.props.processorLoading && this.props.processorsList && this.props.processorsList.length === 0
+            ? <Loader /> : noDataMsg;
     }
 
     componentDidMount() {
@@ -127,7 +122,7 @@ class getProcessors extends React.Component {
     }
 
     render() {
-        const {sortedProcessors, sortHeader} = this.state;
+        const { sortedProcessors, sortHeader } = this.state;
         const noDataText = this.props.getProcessorsFailure ? errorMsgGetProcessors : this.loader();
         const columns = getColumns({
             columnsNames,
@@ -141,13 +136,13 @@ class getProcessors extends React.Component {
         });
         return (
             <Page title={'Processors'} description={DESCRIPTION}>
-                <Button className={style['create-button']} onClick={() => {
+            <Button className={style['create-button']} onClick={() => {
                     this.setState({
                         createProcessor: true
                     });
                 }}>Create Processor</Button>
-                <ReactTableComponent
-                    onSearch={this.onSearch}
+            <ReactTableComponent
+                onSearch={this.onSearch}
                     rowHeight={'46px'}
                     manual={false}
                     data={sortedProcessors}
@@ -160,23 +155,23 @@ class getProcessors extends React.Component {
                     // className={style.table}
                 />
 
-                {this.state.createProcessor &&
-                <ProcessorForm data={this.state.processorForEdit} closeDialog={this.closeCreateTest}/>}
+            {this.state.createProcessor &&
+                <ProcessorForm data={this.state.processorForEdit} closeDialog={this.closeCreateTest} />}
 
                 {(this.state.deleteDialog && !this.props.deleteProcessorSuccess)
                     ? <DeleteDialog loader={this.props.processingDeleteTest}
-                                    display={this.state.processorToDelete ? this.state.processorToDelete.name : ''}
-                                    onSubmit={this.submitDelete}
-                                    onCancel={this.cancelDelete}/> : null}
+                      display={this.state.processorToDelete ? this.state.processorToDelete.name : ''}
+                      onSubmit={this.submitDelete}
+                      onCancel={this.cancelDelete} /> : null}
                 <Snackbar
                     open={this.props.deleteProcessorSuccess}
-                    bodyStyle={{backgroundColor: '#2fbb67'}}
+                    bodyStyle={{ backgroundColor: '#2fbb67' }}
                     message={'Processor deleted successfully'}
                     autoHideDuration={4000}
                     onRequestClose={this.handleSnackbarClose}
                 />
-            </Page>
-        )
+          </Page>
+        );
     }
 }
 
@@ -185,7 +180,7 @@ function mapStateToProps(state) {
         processorsList: Selectors.processorsList(state),
         deleteProcessorSuccess: Selectors.deleteProcessorSuccess(state),
         processorLoading: Selectors.processorsLoading(state)
-    }
+    };
 }
 
 const mapDispatchToProps = {
