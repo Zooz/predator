49c0bde6f798c672597d2b626b1e57a716d538d9
Manor <manor.lahagani@zooz.com>
2020-03-15 21:18:36 +0200

    fix ui bugs

--- ui/src/components/FormWrapper/index.js
+++ ui/src/components/FormWrapper/index.js
@@ -3,10 +3,10 @@ import style from './style.scss'
 import classnames from 'classnames';
 
 
-const FormWrapper = ({children,title,description})=>{
+const FormWrapper = ({children, title, description, style: customStyle}) => {
 
     return (
-            <div style={{display:'flex',flexDirection:'column',height:'100%'}}>
+        <div style={{display: 'flex', flexDirection: 'column', height: '100%',...customStyle}}>
                 <div className={style.title}>{title}</div>
                 {description && <div className={style.description}>{description}</div>}
                 <div className={style.content}>
