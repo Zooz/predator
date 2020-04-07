f2c5726f5a8d73e18a49a9ec6505d09209a5f070
frenkel <or@zooz.com>
2020-04-07 10:27:20 +0300

    feat(data-type): change type Sequelize.DataTypes.TEXT('long')

--- src/reports/models/database/sequelize/sequelizeConnector.js
+++ src/reports/models/database/sequelize/sequelizeConnector.js
@@ -266,7 +266,7 @@ async function initSchemas() {
             type: Sequelize.DataTypes.STRING
         },
         benchmark_weights_data: {
-            type: Sequelize.DataTypes.BLOB
+            type: Sequelize.DataTypes.TEXT('long')
         },
         score: {
             type: Sequelize.DataTypes.FLOAT
