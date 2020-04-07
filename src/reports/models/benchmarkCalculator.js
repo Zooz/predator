b604cbbc972af55275da39fe0d3ba3d792836a65
frenkel <or@zooz.com>
2020-04-07 14:45:42 +0300

    test(): remove comments

--- src/reports/models/benchmarkCalculator.js
+++ src/reports/models/benchmarkCalculator.js
@@ -55,89 +55,13 @@ function extractFieldScore(benchmarkObject, reportObject, field) {
     const isRps = field === RPS;
     const expectedHigherValue = isRps ? reportObject[field] : benchmarkObject[field];
     const expectedLowerValue = isRps ? benchmarkObject[field] : reportObject[field];
-    return calaualteFieldScore(expectedHigherValue, expectedLowerValue);
+    return calculateFieldScore(expectedHigherValue, expectedLowerValue);
 }
 
-function calaualteFieldScore(expectedHigherValue, expectedLowerValue) {
+function calculateFieldScore(expectedHigherValue, expectedLowerValue) {
     if (expectedHigherValue >= expectedLowerValue) {
         return 100;
     }
     const percentage = expectedHigherValue / expectedLowerValue;
     return (percentage * 100);
 }
-
-// const object =
-//     {
-//         'rps': {
-//             'mean': 9
-//         },
-//         'errors': {},
-//         'codes': {},
-//         'latency': {
-//             'p95': 100,
-//             'median': 150
-//         }
-//     };
-//
-// const object2 = {
-//     'bucket': 60,
-//     'requestsCompleted': 118,
-//     'scenariosCreated': 60,
-//     'scenariosAvoided': 0,
-//     'scenariosCompleted': 59,
-//     'pendingRequests': 1,
-//     'scenarioCounts': {
-//         'Scenario 1': 60
-//     },
-//     'errors': {},
-//     'concurrency': 1,
-//     'codes': {
-//         '200': 59,
-//         '301': 59
-//     },
-//     'latency': {
-//         'median': 206.10000000000002,
-//         'max': 591.9,
-//         'min': 152.6,
-//         'p95': 248.13728813559322,
-//         'p99': 410.935593220339
-//     },
-//     'rps': {
-//         'mean': 2.02,
-//         'count': 119
-//     },
-//     'scenarioDuration': {
-//         'median': 498.8,
-//         'min': 427.5,
-//         'max': 836.7,
-//         'p95': 548.1779661016949,
-//         'p99': 709.8864406779661
-//     }
-// };
-//
-// const config =
-//     {
-//         'percentile_ninety': {
-//             'factor': 10,
-//             'percentage': 20
-//         },
-//         'percentile_fifty': {
-//             'factor': 10,
-//             'percentage': 30
-//         },
-//         'server_errors': {
-//             'factor': 10,
-//             'percentage': 20
-//         },
-//         'client_errors': {
-//             'factor': 10,
-//             'percentage': 20
-//         },
-//         'rps': {
-//             'factor': 10,
-//             'percentage': 10
-//         }
-//     };
-//
-// const res = this.calculate(object, object2, config);
-// console.log(res);
