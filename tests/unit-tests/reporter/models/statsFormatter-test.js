24e7ad2f8e6d1b065d0e87a4941216fcf97b0a52
frenkel <or@zooz.com>
2020-04-12 17:22:23 +0300

    feat(): change webhook style

--- tests/unit-tests/reporter/models/statsFormatter-test.js
+++ tests/unit-tests/reporter/models/statsFormatter-test.js
@@ -65,7 +65,8 @@ describe('Stats formatter test', () => {
     });
     it('Test successful format with benchmark', () => {
         let statsFormatted = statsFormatter.getStatsFormatted('summary', REPORT, { score: 99.6234 });
-        statsFormatted.should.eql('*Score:* 99.62, *RPS sent* 19.15, Scenarios launched: 96, Scenarios completed: 92, Scenarios avoided: 50, Requests completed: 185.\n' +
+        statsFormatted.should.eql('*Score:* 99.62\n' +
+            '*RPS sent* 19.15, Scenarios launched: 96, Scenarios completed: 92, Scenarios avoided: 50, Requests completed: 185.\n' +
             '*Request latency:* min 167.6 max 667.5, median 193.8, p95 322.4, p99 609.6.\n' +
             '*Scenario duration:* min 367.7, max 1115.1, median 385.1, p95 780.8, p99 1060.9.\n' +
             '*Codes:* 201: 185.\n' +
