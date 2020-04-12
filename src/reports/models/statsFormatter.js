b2ad3dc8cfb538d89f6655faac13e18460b2d48a
frenkel <or@zooz.com>
2020-04-12 17:22:23 +0300

    feat(): change webhook style

--- src/reports/models/statsFormatter.js
+++ src/reports/models/statsFormatter.js
@@ -4,7 +4,7 @@ let _ = require('lodash');
 
 module.exports.getStatsFormatted = (stage, report, reportBenchmark = {}) => {
     let beautyReport = '';
-    beautyReport += reportBenchmark.score ? `*Score:* ${reportBenchmark.score.toFixed(2)}, ` : '';
+    beautyReport += reportBenchmark.score ? `*Score:* ${reportBenchmark.score.toFixed(2)}\n` : '';
     beautyReport += `*RPS sent* ${report.rps.mean}`;
     beautyReport += `, Scenarios launched: ${report.scenariosCreated}`;
     beautyReport += `, Scenarios completed: ${report.scenariosCompleted}`;
