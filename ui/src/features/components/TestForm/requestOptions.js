e6d311560b1632c756e54c5e31f14b3c2e1fddf0
Manor <manor.lahagani@zooz.com>
2020-05-04 15:06:44 +0300

    fix(align keep  alive\): align keep  alive

--- ui/src/features/components/TestForm/requestOptions.js
+++ ui/src/features/components/TestForm/requestOptions.js
@@ -1,49 +1,48 @@
-
 import React from 'react';
 
 import UiSwitcher from '../../../components/UiSwitcher'
 
-const SideLabel = ({label,children})=>{
+const SideLabel = ({label, children}) => {
 
 
     return (
         <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
             {children}
-            <div style={{fontSize:'13px',color: '#555555',marginLeft:'10px'}}>{label}</div>
+            <div style={{fontSize: '13px', color: '#555555', marginLeft: '10px'}}>{label}</div>
         </div>
-        )
+    )
 }
 export default (props) => {
-  const { gzipValue, foreverValue, onGzipToggleChanged, onForeverToggleChanged } = props;
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column',justifyContent:'sapce-between',height:'61px',marginLeft:'17px'}}>
-        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-            <SideLabel label={'gzip'}>
-                <UiSwitcher
-                    onChange={(value) => {
-                        onGzipToggleChanged(value)
-                    }}
-                    disabledInp={false}
-                    activeState={!!gzipValue}
-                    height={12}
-                    width={22}
-                />
-            </SideLabel>
-      </div>
-        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-            <SideLabel label={'keep-alive'}>
-                <UiSwitcher
-                    onChange={(value) => {
-                        onForeverToggleChanged(value)
-                    }}
-                    disabledInp={false}
-                    activeState={!!foreverValue}
-                    height={12}
-                    width={22}
-                />
-            </SideLabel>
-      </div>
-    </div>
+    const {gzipValue, foreverValue, onGzipToggleChanged, onForeverToggleChanged} = props;
+    return (
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'sapce-between', marginLeft: '17px'}}>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <SideLabel label={'gzip'}>
+                    <UiSwitcher
+                        onChange={(value) => {
+                            onGzipToggleChanged(value)
+                        }}
+                        disabledInp={false}
+                        activeState={!!gzipValue}
+                        height={12}
+                        width={22}
+                    />
+                </SideLabel>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <SideLabel label={'keep-alive'}>
+                    <UiSwitcher
+                        onChange={(value) => {
+                            onForeverToggleChanged(value)
+                        }}
+                        disabledInp={false}
+                        activeState={!!foreverValue}
+                        height={12}
+                        width={22}
+                    />
+                </SideLabel>
+            </div>
+        </div>
 
-  )
+    )
 }
