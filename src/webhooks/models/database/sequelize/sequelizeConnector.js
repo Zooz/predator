53f929087ce3b901e69016b58124169e5aad423b
Daniel Hermon <danielhermonsync@gmail.com>
2020-06-15 10:23:30 +0300

    implement GET /webhooks (#318)

---
+++ src/webhooks/models/database/sequelize/sequelizeConnector.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+let client;
+
+module.exports = {
+    init,
+    getAllWebhooks
+};
+
+async function init(sequelizeClient) {
+    client = sequelizeClient;
+    await initSchemas();
+}
+
+async function getAllWebhooks() {
+    const webhooksModel = client.model('webhook');
+    return webhooksModel.findAll({ include: ['events'] });
+}
+
+async function initSchemas() {
+    const webhooksSchema = client.define('webhook', {
+        id: {
+            type: Sequelize.DataTypes.UUID,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.DataTypes.TEXT('medium')
+        },
+        url: {
+            type: Sequelize.DataTypes.STRING
+        },
+        global: {
+            type: Sequelize.DataTypes.BOOLEAN
+        },
+        format_type: {
+            type: Sequelize.DataTypes.STRING
+        },
+        created_at: {
+            type: Sequelize.DataTypes.DATE
+        },
+        updated_at: {
+            type: Sequelize.DataTypes.DATE
+        }
+    });
+    const webhooksEvents = client.define('webhook_event', {
+        id: {
+            type: Sequelize.DataTypes.UUID,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.DataTypes.TEXT('medium')
+        }
+    });
+
+    webhooksSchema.belongsToMany(webhooksEvents, {
+        through: 'webhook_event_mapping',
+        as: 'events',
+        foreignKey: 'webhook_id'
+    });
+    webhooksEvents.belongsToMany(webhooksSchema, {
+        through: 'webhook_event_mapping',
+        as: 'webhooks',
+        foreignKey: 'webhook_event_id'
+    });
+
+    await webhooksSchema.sync();
+    await webhooksEvents.sync();
+}
