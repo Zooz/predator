ee138aa470cbfb17fdcf884d8ecad72004b7228d
Daniel Hermon <danielhermonsync@gmail.com>
2020-06-22 11:50:59 +0300

    feat(webhooks): implement POST /webhooks (#319)

--- src/webhooks/routes/webhooksRouter.js
+++ src/webhooks/routes/webhooksRouter.js
@@ -7,5 +7,6 @@ let router = express.Router();
 let webhooksController = require('../controllers/webhooksController');
 
 router.get('/', swaggerValidator.validate, webhooksController.getAllWebhooks);
+router.post('/', swaggerValidator.validate, webhooksController.createWebhook);
 
 module.exports = router;
