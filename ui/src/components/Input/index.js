5e0ac57056908e0f99e3abed18544b086c9ce418
manorlh <44364426+manorlh@users.noreply.github.com>
2020-09-09 14:45:10 +0300

    feat(ui-step-name): ui-step-name (#371)
    * feat(ui): add a name to requests

--- ui/src/components/Input/index.js
+++ ui/src/components/Input/index.js
@@ -3,27 +3,27 @@ import classnames from 'classnames'
 import PropTypes from 'prop-types'
 import css from './Input.scss'
 
-const Input = React.forwardRef(({error, className, height, ...rest}, ref) => {
-  const classes = classnames(className, css['input'], {
-    [css['input--error']]: error,
-    [css['input--disabled']]: rest.disabled
-  })
-  return (
-    <input ref={ref} style={{
-      '--input-height': height
-    }} className={classes} {...rest} />
-  )
+const Input = React.forwardRef(({error, className, height, value, ...rest}, ref) => {
+    const classes = classnames(className, css['input'], {
+        [css['input--error']]: error,
+        [css['input--disabled']]: rest.disabled
+    })
+    return (
+        <input ref={ref} value={value} style={{
+            '--input-height': height
+        }} className={classes} {...rest} />
+    )
 })
 
 Input.propTypes = {
-  error: PropTypes.bool,
-  disabled: PropTypes.bool,
-  height: PropTypes.string,
-  className: PropTypes.string
+    error: PropTypes.bool,
+    disabled: PropTypes.bool,
+    height: PropTypes.string,
+    className: PropTypes.string
 }
 
 Input.defaultProps = {
-  height: '35px'
+    height: '35px'
 }
 
 export default Input
