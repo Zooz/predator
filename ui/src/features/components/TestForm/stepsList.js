d0fa7b604d784ed8af3270f60a896497318e28bf
manor lahagani <manorlahagani@manors-MacBook-Pro.local>
2020-10-07 20:51:41 +0300

    fix(ui-fix-collapsible-on-edit-test): ui-fix-collapsible-on-edit-test
    ui-fix-collapsible-on-edit-test

--- ui/src/features/components/TestForm/stepsList.js
+++ ui/src/features/components/TestForm/stepsList.js
@@ -2,59 +2,58 @@ import React from 'react';
 import CollapsibleStep from './collapsibleStep';
 import DragableWrapper from './dragableWrapper'
 export default class StepsList extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor (props) {
+    super(props);
+  }
 
-    render() {
-        const {
-            onChangeValueOfStep,
-            processorsExportedFunctions,
-            onBeforeStepProcessorChange,
-            onAfterStepProcessorChange,
-            beforeStepProcessorValue,
-            afterStepProcessorValue,
-            onDeleteStep,
-            onDuplicateStep,
-            updateStepOrder,
-        } = this.props;
-        return (<div style={{
-            width: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            flexDirection: 'column',
-        }}>
+  render () {
+    const {
+      onChangeValueOfStep,
+      processorsExportedFunctions,
+      onBeforeStepProcessorChange,
+      onAfterStepProcessorChange,
+      beforeStepProcessorValue,
+      afterStepProcessorValue,
+      onDeleteStep,
+      onDuplicateStep,
+      updateStepOrder,
+      editMode
+    } = this.props;
+    return (<div style={{
+      width: '100%',
+      display: 'flex',
+      alignItems: 'center',
+      flexDirection: 'column'
+    }}>
 
-            {
-                this.props.steps.map((step,index) => {
+      {
+        this.props.steps.map((step, index) => {
+          return (
+            <DragableWrapper
+              key={step.id}
+              index={index}
+              id={step.id}
+              move={updateStepOrder}
+            >
+              <CollapsibleStep
+                expandedOnStart={!editMode}
+                index={index}
+                key={index}
+                step={step}
+                onChangeValueOfStep={onChangeValueOfStep}
+                processorsExportedFunctions={processorsExportedFunctions}
+                onBeforeStepProcessorChange={onBeforeStepProcessorChange}
+                onAfterStepProcessorChange={onAfterStepProcessorChange}
+                beforeStepProcessorValue={beforeStepProcessorValue}
+                afterStepProcessorValue={afterStepProcessorValue}
+                onDeleteStep={onDeleteStep}
+                onDuplicateStep={onDuplicateStep}
+              />
+            </DragableWrapper>
+          )
+        })
 
-                    return (
-                           <DragableWrapper
-                               key={step.id}
-                               index={index}
-                               id={step.id}
-                               move={updateStepOrder}
-                           >
-                            <CollapsibleStep
-                                index={index}
-                                key={index}
-                                step={step}
-                                onChangeValueOfStep={onChangeValueOfStep}
-                                processorsExportedFunctions={processorsExportedFunctions}
-                                onBeforeStepProcessorChange={onBeforeStepProcessorChange}
-                                onAfterStepProcessorChange={onAfterStepProcessorChange}
-                                beforeStepProcessorValue={beforeStepProcessorValue}
-                                afterStepProcessorValue={afterStepProcessorValue}
-                                onDeleteStep={onDeleteStep}
-                                onDuplicateStep={onDuplicateStep}
-                            />
-                           </DragableWrapper>
-                    )
-                })
-
-            }
-        </div>)
-
-    }
-
+      }
+    </div>)
+  }
 }
