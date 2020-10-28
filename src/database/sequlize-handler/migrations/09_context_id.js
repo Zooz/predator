47313b40514825f8afced6b1ebbf628a16077c40
NivLipetz <niv.lipetz@gmail.com>
2020-10-28 19:11:28 +0200

    fix: sequelize migration

--- src/database/sequlize-handler/migrations/09_context_id.js
+++ src/database/sequlize-handler/migrations/09_context_id.js
@@ -8,17 +8,29 @@ const predatorTables = ['tests', 'jobs', 'reports', 'dsl_definitions', 'webhooks
 
 module.exports.up = async (query) => {
     for(const table of predatorTables) {
+        const transaction = await query.sequelize.transaction();
+
         const sequelizeTable = await query.describeTable(table);
-        if (!sequelizeTable.context_id) {
-            await query.addColumn(
-                table, CONTEXT_ID,
-                Sequelize.DataTypes.STRING);
+        try {
+            if (!sequelizeTable.context_id) {
+                await query.addColumn(
+                    table,
+                    CONTEXT_ID,
+                    Sequelize.DataTypes.STRING, { transaction });
+                await query.addIndex(
+                    table,
+                    [CONTEXT_ID], { transaction });
+
+                await transaction.commit();
+            }
+        } catch (err) {
+            await transaction.rollback();
+            throw err;
         }
     }
 };
 
 module.exports.down = async (query) => {
-    for(const table of predatorTables) {
-        await query.removeColumn(table, CONTEXT_ID);
-    }
+
+
 };
