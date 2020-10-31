2491035530251525b3df0097c7888b35affe6428
NivLipetz <niv.lipetz@gmail.com>
2020-10-31 18:43:58 +0200

    fix: context_id migration

--- src/database/sequlize-handler/migrations/09_context_id.js
+++ src/database/sequlize-handler/migrations/09_context_id.js
@@ -7,30 +7,33 @@ const {
 const predatorTables = ['tests', 'jobs', 'reports', 'dsl_definitions', 'webhooks', 'files', 'processors', 'benchmarks'];
 
 module.exports.up = async (query) => {
+    const transaction = await query.sequelize.transaction();
     for(const table of predatorTables) {
-        const transaction = await query.sequelize.transaction();
-
-        const sequelizeTable = await query.describeTable(table);
-        try {
-            if (!sequelizeTable.context_id) {
-                await query.addColumn(
-                    table,
-                    CONTEXT_ID,
-                    Sequelize.DataTypes.STRING, { transaction });
-                await query.addIndex(
-                    table,
-                    [CONTEXT_ID], { transaction });
-
-                await transaction.commit();
-            }
-        } catch (err) {
-            await transaction.rollback();
-            throw err;
-        }
+        await migrateTable(query, table, transaction);
     }
 };
 
 module.exports.down = async (query) => {
 
-
 };
+
+async function migrateTable(query, table, transaction) {
+    const sequelizeTable = await query.describeTable(table);
+    try {
+        if (!sequelizeTable.context_id) {
+            await query.addColumn(
+                table,
+                CONTEXT_ID,
+                Sequelize.DataTypes.STRING, { transaction });
+            await query.addIndex(
+                table,
+                [CONTEXT_ID], { transaction });
+
+            await transaction.commit();
+            transaction = null;
+        }
+    } catch (err) {
+        await transaction.rollback();
+        throw err;
+    }
+}
