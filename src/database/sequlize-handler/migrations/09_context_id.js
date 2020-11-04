236403029a95b39df0c3d7f1e2f75238d4cf31c4
NivLipetz <niv.lipetz@gmail.com>
2020-11-04 21:03:23 +0200

    fix: context_id index migration

--- src/database/sequlize-handler/migrations/09_context_id.js
+++ src/database/sequlize-handler/migrations/09_context_id.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 
-const {
-    CONTEXT_ID
-} = require('../../../common/consts');
+const { CONTEXT_ID } = require('../../../common/consts'),
+    logger = require('../../../common/logger');
 
 const predatorTables = ['tests', 'jobs', 'reports', 'dsl_definitions', 'webhooks', 'files', 'processors', 'benchmarks'];
 
@@ -19,19 +18,30 @@ module.exports.down = async (query) => {
 
 async function migrateTable(query, table, transaction) {
     const sequelizeTable = await query.describeTable(table);
+
     try {
         if (!sequelizeTable.context_id) {
             await query.addColumn(
                 table,
                 CONTEXT_ID,
                 Sequelize.DataTypes.STRING, { transaction });
-            await query.addIndex(
-                table,
-                [CONTEXT_ID], { transaction });
         }
+
+        await addIndex(query, table, transaction);
+
         await transaction.commit();
     } catch (err) {
         await transaction.rollback();
         throw err;
+    }
+}
+
+async function addIndex(query, table, transaction) {
+    try {
+        await query.addIndex(
+            table,
+            [CONTEXT_ID], { transaction });
+    } catch (err) {
+        logger.warn(err, 'index already exists')
     }
 }
