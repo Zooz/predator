97d1e75856a6e600cf4294421b8ee49d949dd3ff
Goni <goni.gonen@payu.com>
2021-08-23 15:24:49 +0300

    fix: audit

--- ui/src/App/common/env.js
+++ ui/src/App/common/env.js
@@ -6,13 +6,13 @@ module.exports = {
 };
 
 function generatePredatorUrl() {
-    if (process.env.PREDATOR_URL){
-        return process.env.PREDATOR_URL;
-    }
-    if (process.env.BUCKET_PATH) {
-        return `${process.env.BUCKET_PATH}/v1`;
-    }
-    return '/v1';
+    // if (process.env.PREDATOR_URL){
+    //     return process.env.PREDATOR_URL;
+    // }
+    // if (process.env.BUCKET_PATH) {
+    //     return `${process.env.BUCKET_PATH}/v1`;
+    // }
+    return 'http://predator.eks-mars-apps.zooz.co/v1';
 }
 
 function generateBucketPath() {
