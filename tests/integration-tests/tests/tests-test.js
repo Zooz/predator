873baffa2a5ba67c5c713cf3fc2084f265a68d49
kerenfinkelstein <keren.filkenstein@payu.com>
2024-07-03 21:07:14 +0300

    test: fix Should return error for file url not exists

--- tests/integration-tests/tests/tests-test.js
+++ tests/integration-tests/tests/tests-test.js
@@ -47,7 +47,7 @@ describe('the tests api', function() {
                 });
         });
         it('Should return error for file url not exists ', async () => {
-            const requestBody = Object.assign({ processor_file_url: 'https://www.notRealUrl.com' }, simpleTest.test);
+            const requestBody = Object.assign({ processor_file_url: 'https://www.notRealUrl123.com' }, simpleTest.test);
             const res = await testsRequestSender.createTest(requestBody, validHeaders);
             res.statusCode.should.eql(422);
             res.body.message.should.containEql('Error to download file: RequestError');
