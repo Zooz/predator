b302a2811cb5d151eb091df8c7aa4f5274854b96
Guy Abramovich <guy.abramovich@payu.com>
2024-07-10 17:08:06 +0300

    handler test

--- tests/unit-tests/jobs/models/jobExperimentsHandler-test.js
+++ tests/unit-tests/jobs/models/jobExperimentsHandler-test.js
@@ -1,4 +1,6 @@
 const sinon = require('sinon'),
+    rewire = require('rewire'),
+    should = require('should'),
     jobExperimentHandler = require('../../../../src/jobs/models/kubernetes/jobExperimentsHandler'),
     chaosExperimentsManager = require('../../../../src/chaos-experiments/models/chaosExperimentsManager');
 const { v4: uuid } = require('uuid');
@@ -25,6 +27,7 @@ describe('Job experiments handler tests', function () {
     let experimentsManagerInsertStub;
     let experimentsManagerGetStub;
     let experimentsManagerRunJobStub;
+    let stopJobExperimentsByJobIdStub;
     let sandbox;
     let clock;
 
@@ -33,6 +36,7 @@ describe('Job experiments handler tests', function () {
         experimentsManagerInsertStub = sandbox.stub(chaosExperimentsManager, 'insertChaosJobExperiment');
         experimentsManagerGetStub = sandbox.stub(chaosExperimentsManager, 'getChaosExperimentsByIds');
         experimentsManagerRunJobStub = sandbox.stub(chaosExperimentsManager, 'runChaosExperiment');
+        stopJobExperimentsByJobIdStub = sandbox.stub(chaosExperimentsManager, 'stopJobExperimentsByJobId');
     });
     beforeEach(async () => {
         sandbox.reset();
@@ -49,70 +53,87 @@ describe('Job experiments handler tests', function () {
         sandbox.restore();
     });
 
-    it('set chaos experiments with 2 experiments', async () => {
-        const firstExperiment = generateExperiment();
-        const secondExperiment = generateExperiment();
-        const firstExperimentName = firstExperiment.kubeObject.metadata.name;
-        experimentsManagerInsertStub.resolves();
-        experimentsManagerGetStub.resolves([firstExperiment, secondExperiment]);
-        experimentsManagerRunJobStub.resolves();
-        const jobExperiments = [
-            {
-                experiment_id: firstExperiment.id,
-                start_after: 1000
-            },
-            {
-                experiment_id: secondExperiment.id,
-                start_after: 2000
-            }
-        ];
-        const jobId = uuid();
-        clock = sinon.useFakeTimers();
-        clock.tick(1000);
-        await jobExperimentHandler.setChaosExperimentsIfExist(jobId, jobExperiments);
-        clock.tick(3000);
-        (experimentsManagerRunJobStub.callCount).should.eql(2);
-        experimentsManagerRunJobStub.args[0][0].metadata.name.should.eql(`${firstExperimentName}-${experimentsManagerRunJobStub.args[0][2]}`);
-        experimentsManagerGetStub.callCount.should.eql(1);
-        experimentsManagerInsertStub.callCount.should.eql(2);
-        experimentsManagerInsertStub.args[0][1].should.eql(jobId);
-        experimentsManagerInsertStub.args[0][2].should.eql(firstExperiment.id);
-        (experimentsManagerInsertStub.args[0][4] - experimentsManagerInsertStub.args[0][3]).should.eql(60000);
-        experimentsManagerInsertStub.args[1][1].should.eql(jobId);
-        experimentsManagerInsertStub.args[1][2].should.eql(secondExperiment.id);
-        (experimentsManagerInsertStub.args[1][3] - experimentsManagerInsertStub.args[0][3]).should.eql(1000);
-    });
+    describe('Set experiments if exist tests', function () {
+        it('set chaos experiments with 2 experiments', async () => {
+            const firstExperiment = generateExperiment();
+            const secondExperiment = generateExperiment();
+            const firstExperimentName = firstExperiment.kubeObject.metadata.name;
+            experimentsManagerInsertStub.resolves();
+            experimentsManagerGetStub.resolves([firstExperiment, secondExperiment]);
+            experimentsManagerRunJobStub.resolves();
+            const jobExperiments = [
+                {
+                    experiment_id: firstExperiment.id,
+                    start_after: 1000
+                },
+                {
+                    experiment_id: secondExperiment.id,
+                    start_after: 2000
+                }
+            ];
+            const jobId = uuid();
+            clock = sinon.useFakeTimers();
+            clock.tick(1000);
+            await jobExperimentHandler.setChaosExperimentsIfExist(jobId, jobExperiments);
+            clock.tick(3000);
+            (experimentsManagerRunJobStub.callCount).should.eql(2);
+            experimentsManagerRunJobStub.args[0][0].metadata.name.should.eql(`${firstExperimentName}-${experimentsManagerRunJobStub.args[0][2]}`);
+            experimentsManagerGetStub.callCount.should.eql(1);
+            experimentsManagerInsertStub.callCount.should.eql(2);
+            experimentsManagerInsertStub.args[0][1].should.eql(jobId);
+            experimentsManagerInsertStub.args[0][2].should.eql(firstExperiment.id);
+            (experimentsManagerInsertStub.args[0][4] - experimentsManagerInsertStub.args[0][3]).should.eql(60000);
+            experimentsManagerInsertStub.args[1][1].should.eql(jobId);
+            experimentsManagerInsertStub.args[1][2].should.eql(secondExperiment.id);
+            (experimentsManagerInsertStub.args[1][3] - experimentsManagerInsertStub.args[0][3]).should.eql(1000);
+        });
 
-    it('set chaos experiments with same experiment in different times', async () => {
-        const experiment = generateExperiment();
-        experimentsManagerInsertStub.resolves();
-        experimentsManagerGetStub.resolves([experiment]);
-        const jobExperiments = [
-            {
-                experiment_id: experiment.id,
-                start_after: 1000
-            },
-            {
-                experiment_id: experiment.id,
-                start_after: 2000
-            }
-        ];
-        const jobId = uuid();
-        const clock = sinon.useFakeTimers();
-        clock.tick(1000);
-        await jobExperimentHandler.setChaosExperimentsIfExist(jobId, jobExperiments);
-        clock.tick(3000);
-        experimentsManagerGetStub.callCount.should.eql(1);
-        experimentsManagerInsertStub.callCount.should.eql(2);
-        experimentsManagerInsertStub.args[0][1].should.eql(jobId);
-        experimentsManagerInsertStub.args[0][2].should.eql(experiment.id);
-        (experimentsManagerInsertStub.args[0][4] - experimentsManagerInsertStub.args[0][3]).should.eql(60000);
-        clock.restore();
-    });
+        it('set chaos experiments with same experiment in different times', async () => {
+            const experiment = generateExperiment();
+            experimentsManagerInsertStub.resolves();
+            experimentsManagerGetStub.resolves([experiment]);
+            const jobExperiments = [
+                {
+                    experiment_id: experiment.id,
+                    start_after: 1000
+                },
+                {
+                    experiment_id: experiment.id,
+                    start_after: 2000
+                }
+            ];
+            const jobId = uuid();
+            const clock = sinon.useFakeTimers();
+            clock.tick(1000);
+            await jobExperimentHandler.setChaosExperimentsIfExist(jobId, jobExperiments);
+            clock.tick(3000);
+            experimentsManagerGetStub.callCount.should.eql(1);
+            experimentsManagerInsertStub.callCount.should.eql(2);
+            experimentsManagerInsertStub.args[0][1].should.eql(jobId);
+            experimentsManagerInsertStub.args[0][2].should.eql(experiment.id);
+            (experimentsManagerInsertStub.args[0][4] - experimentsManagerInsertStub.args[0][3]).should.eql(60000);
+            clock.restore();
+        });
 
-    it('set chaos experiments with no experiments', async () => {
-        await jobExperimentHandler.setChaosExperimentsIfExist(uuid(), []);
-        experimentsManagerGetStub.callCount.should.eql(1);
-        experimentsManagerInsertStub.callCount.should.eql(0);
+        it('set chaos experiments with no experiments', async () => {
+            await jobExperimentHandler.setChaosExperimentsIfExist(uuid(), []);
+            experimentsManagerGetStub.callCount.should.eql(1);
+            experimentsManagerInsertStub.callCount.should.eql(0);
+        });
+    });
+    describe('stop chaos experiments for job', function () {
+        const jobExperimentHandlerRewire = rewire('../../../../src/jobs/models/kubernetes/jobExperimentsHandler');
+        it('set chaos experiments with 2 experiments', async () => {
+            const jobId = uuid();
+            const map = new Map();
+            const timeouts = [setTimeout(() => true, 10000), setTimeout(() => true, 50000)];
+            map.set(jobId, timeouts);
+            jobExperimentHandlerRewire.__set__('jobIdsToTimeouts', map);
+            stopJobExperimentsByJobIdStub.resolves();
+            await jobExperimentHandlerRewire.stopChaosExperimentsForJob(jobId);
+            const updatedMap = jobExperimentHandlerRewire.__get__('jobIdsToTimeouts', timeouts);
+            should(updatedMap.has(jobId)).be.eql(false);
+            stopJobExperimentsByJobIdStub.callCount.should.eql(1);
+        });
     });
 });
