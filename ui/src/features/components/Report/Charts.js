4692543be75f0de90e2c74e1984a0ffb06815e2a
kerenfinkelstein <keren.filkenstein@payu.com>
2024-07-11 15:42:24 +0300

    fix: redundant color param

--- ui/src/features/components/Report/Charts.js
+++ ui/src/features/components/Report/Charts.js
@@ -135,9 +135,8 @@ export const LineChartPredator = ({ data = [], keys = [], labelY, graphType, onS
       {
         experiments.map((experiment, index) => {
           const key = `experiment-${index}`;
-          const color = getColor(key, index);
           return (
-            renderExperimentsReferenceArea(experiment, color, key)
+            renderExperimentsReferenceArea(experiment, key)
           )
         })
       }
